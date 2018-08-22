584e9e8825e6299ba17be4d582a29c919277cf71
Paweł Haracz <haracz5@live.com>
2018-08-22 23:10:14 +0200

    crud operation

---
+++ CRM.Workshops/Examples/Crud.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace CRM.Workshops.Examples
+{
+    public class Crud
+    {
+        private readonly IOrganizationService _organizationService;
+
+        public Crud(IOrganizationService organizationService)
+        {
+            _organizationService = organizationService;
+        }
+
+        public Guid Create(Entity entity)
+        {
+            var id = _organizationService.Create(entity);
+
+            return id;
+        }
+
+        public void Update(Entity entity)
+        {
+            _organizationService.Update(entity);
+        }
+
+
+        public void Delete(EntityReference entityReference)
+        {
+            _organizationService.Delete(entityReference.LogicalName, entityReference.Id);
+        }
+    }
+}
